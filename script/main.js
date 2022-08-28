33ac90d263493e7a2bc2766aca50bc4f79922703
rrrrraphael <raphael.voelker@gmx.at>
2022-08-28 15:32:54 +0200

    template fertig, index.html slideshow angefangen

---
+++ script/main.js
@@ -0,0 +1,9 @@
+//responsive navbar
+function responsiveTopnav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
