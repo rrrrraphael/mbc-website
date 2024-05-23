bfdfca218853bf3c59b6b703b861622fd6a9ffca
rrrrraphael <raphael.voelker@gmx.at>
2024-05-23 20:17:20 +0200

    gallery creator finished

--- gallery_creator/gallery_page_creator.py
+++ gallery_creator/gallery_page_creator.py
@@ -2,6 +2,8 @@ import os
 from os.path import exists
 import tkinter as tk
 from tkinter import filedialog
+import PIL
+from PIL import Image
 
 def create_file(path:str):
     if exists(path):
@@ -23,31 +25,31 @@ def create_header(path:str):
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1">
                     <title>Galerie</title>
-                    <link rel="stylesheet" href="../style/main.css">
-                    <link rel="stylesheet" href="../style/gallery_page.css">
-                    <link rel="icon" type="image/x-icon" href="../img/logoMBC.png">
+                    <link rel="stylesheet" href="../../style/main.css">
+                    <link rel="stylesheet" href="../../style/gallery_page.css">
+                    <link rel="icon" type="image/x-icon" href="../../img/logoMBC.png">
 
                 </head>
                 <body id="body">
 
                     <div class="topnav" id="myTopnav">
-                        <img src="../img/logoMBC.png" alt="logoMBC" class="logo">
-                        <a href="index.html" class="hover-underline-animation">Home</a>
-                        <a href="anmeldung.html" class="hover-underline-animation">Anmeldung</a>
+                        <img src="../../img/logoMBC.png" alt="logoMBC" class="logo">
+                        <a href="../index.html" class="hover-underline-animation">Home</a>
+                        <a href="../anmeldung.html" class="hover-underline-animation">Anmeldung</a>
                         <div class="dropdown">
-                            <button class="dropbtn hover-underline-animation" onclick="window.location.href='about.html'">Über uns 
+                            <button class="dropbtn hover-underline-animation" onclick="window.location.href='../about.html'">Über uns 
                             </button>
                             <div class="dropdown-content">
-                                <a href="flugzeiten.html">Flugzeiten</a>
-                                <a href="webcam.html">Webcam</a>
-                                <a href="flugplatzordnung.html">Flugplatzordnung</a>
-                                <a href="betrieb.html">Betrieb</a>
-                                <a href="laermessung.html">Lärmmessung</a>
+                                <a href="../flugzeiten.html">Flugzeiten</a>
+                                <a href="../webcam.html">Webcam</a>
+                                <a href="../flugplatzordnung.html">Flugplatzordnung</a>
+                                <a href="../betrieb.html">Betrieb</a>
+                                <a href="../laermessung.html">Lärmmessung</a>
                             </div>
                         </div> 
-                        <a href="neuigkeiten.html" class="hover-underline-animation">Neuigkeiten</a>
-                        <a href="gallerie.html" class="hover-underline-animation">Gallerie</a>
-                        <a href="kontakt.html" class="hover-underline-animation">Kontakt</a>
+                        <a href="../neuigkeiten.html" class="hover-underline-animation">Neuigkeiten</a>
+                        <a href="../gallerie.html" class="hover-underline-animation">Galerie</a>
+                        <a href="../kontakt.html" class="hover-underline-animation">Kontakt</a>
 
                         <a href="javascript:void(0);" class="icon" onclick="responsiveTopnav()">&#9776;</a>
                     </div>
@@ -79,6 +81,11 @@ def create_footer(path:str):
     if( exists(path)):
         file = open(path, 'a')
         file.write("""
+            <div id="overlay" onclick="closeImageViewer()"></div>
+            <div id="imageViewer">
+                <img id="imageViewedImg" />
+            </div>
+
             <footer>
                 <p class="footerHeader">Infos</p>
                 <address>Addresse : Modellbauclub Erlauftal, Moos 4, 3250 Wieselburg. </address>
@@ -89,12 +96,12 @@ def create_footer(path:str):
             </footer>
 
 
-            <script src="../script/main.js">
+            <script src="../../script/main.js">
 
             </script>
         </body>
 
-        <script src="../script/gallery_page.js">    </script>
+        <script src="../../script/gallery_page.js">    </script>
 </html>
             """)
         file.close()
@@ -119,13 +126,14 @@ def create_gallery(img_path:str, path:str):
         i = 0
         j = 0
         length = len(img_paths)
-        length -=2
+        length -= 1
         while (i < length):
             content += """            <div class="imgRow">\n"""
-            while(j < 4):
+            while(j < 4 and i < length):
                 content += """                <div class="imgDiv box"><img src=""" + '"'   + img_paths[i] + '"' + """ class="gallery-image" /></div>\n"""
-                i+=1
                 j+=1
+                i+=1
+
             content += """            </div>\n"""
             j = 0
         content += """        </div>"""
@@ -135,11 +143,26 @@ def create_gallery(img_path:str, path:str):
     else:
         print("files not existing")
 
+def optimize_images(path, quality):
+    image_names = []
+    for filename in os.listdir(path):
+    #Check if file is iamge (.jpg, .png , ...)
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            #Add to array
+            image_names.append(path + "/" + filename)
+    for image in image_names:
+        img = Image.open(image)
+        #Resize and compress image
+        img = img.resize(img.size, PIL.Image.Resampling.LANCZOS)
+        #Replace old image with optimization and lower quality
+        img.save(image, optimize=True, quality=quality)
+
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.withdraw()
-    path = filedialog.askdirectory()
+    #path = filedialog.askdirectory()
+    path = "D:\Webentwicklung\mbc-website\html\gallery_pages"
     print('Dateiname: ')
     filename = input()
     filepath = path + '/' + filename + '.html'
@@ -149,6 +172,10 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.withdraw()
     imgs_path = filedialog.askdirectory()
+    print("Want to optimize images (Y/n)?")
+    result = input()
+    if result == "Y" or result == "y":
+        optimize_images(imgs_path, 85)
     create_gallery(imgs_path, filepath)
     create_footer(filepath)
 
